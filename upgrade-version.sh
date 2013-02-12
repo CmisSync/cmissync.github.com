63b3d4d8b41f5fa887bfa0e37143c23b316e233a
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-02-12 17:00:30 +0900

    Bumped version

--- upgrade-version.sh
+++ upgrade-version.sh
@@ -1,4 +1,4 @@
-find . -name "*.html" -print | xargs sed -i 's/0\.3\.8/0\.3\.9/g'
+find . -name "*.html" -print | xargs sed -i 's/0\.3\.9/0\.4\.0/g'
 
 git add alfresco/index.html google-drive/index.html index.html nuxeo/index.html upgrade-version.sh
 git commit -m "Bumped version"
