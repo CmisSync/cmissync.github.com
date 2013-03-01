52986a9a9d1726111721b5a77f0290b2ba78e306
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-03-01 14:42:55 +0900

    image used on CmisSync wiki

--- upgrade-version.sh
+++ upgrade-version.sh
@@ -1,7 +1,8 @@
-find . -name "*.html" -print | xargs sed -i 's/0\.4\.2/0\.4\.3/g'
+find . -name "*.html" -print | xargs sed -i 's/0\.4\.3/0\.3\.9/g'
 
-# Push all HTML files
+# Push all HTML files, and self
 git ls-files . | grep '\.html$' | xargs git add
+git add upgrade-version.sh
 
 git commit -m "Bumped version"
 git push
