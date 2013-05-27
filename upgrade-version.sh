22d91770e7b7e015ed59525ece94a4f44c493462
Nicolas Raoul <nicolas.raoul@gmail.com>
2013-05-27 19:21:58 +0900

    Bumped version

--- upgrade-version.sh
+++ upgrade-version.sh
@@ -1,4 +1,4 @@
-find . -name "*.html" -print | xargs sed -i 's/0\.4\.12/0\.4\.13/g'
+find . -name "*.html" -print | xargs sed -i 's/0\.4\.13/0\.4\.14/g'
 
 # Push all HTML files, and self
 git ls-files . | grep '\.html$' | xargs git add
