ac3d832de1630169ce7ca4ea64420ed5867bacd0
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-08-12 16:17:02 +0900

    As tipped by Florian, take number of capabilities into account https://twitter.com/florian_mueller/status/490407494691602432

--- CmisCompat/score.sh
+++ CmisCompat/score.sh
@@ -6,10 +6,12 @@ SKIPPED=`grep "\"tckResultSKIPPED\"" $TCK | wc -l`
 WARNING=`grep "\"tckResultWARNING\"" $TCK | wc -l`
 FAILURE=`grep "\"tckResultFAILURE\"" $TCK | wc -l`
 EXCEPTION=`grep "\"tckResultUNEXPECTED_EXCEPTION\"" $TCK | wc -l`
+CAPABILITIES=`grep "capability: " $TCK | grep -v "capability: false" | grep -v "capability: NONE" | wc -l`
 
-SCORE=$(expr $OK - $EXCEPTION)
+let SCORE=" ($OK - $EXCEPTION) * $CAPABILITIES "
 
 echo "SCORE: $SCORE"
+echo "CAPABILITIES: $CAPABILITIES"
 echo "INFO: $INFO"
 echo "OK: $OK"
 echo "SKIPPED: $SKIPPED"
